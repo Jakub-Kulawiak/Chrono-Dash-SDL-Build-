b0442ee857d227415b5e4db7de22610d20d28b1b
Jakub Kulawiak <Jakub.Kulawiak@georgebrown.ca>
2021-01-22 15:00:53 -0500

    Added blank engine
    For a starting point.

---
+++ GAME1007_SDLTemplate/Engine.cpp
@@ -0,0 +1,141 @@
+#include "Engine.h"
+#include "SDL_image.h"
+
+
+int Engine::Init(const char* title, int xPos, int yPos, int width, int height, int flags)
+{
+	cout << "Initializing engine..." << endl;
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) // If initialization is okay...
+	{
+		// Create the SDL window...
+		
+		m_pWindow = SDL_CreateWindow(title, xPos, yPos, width, height, flags);
+		if (m_pWindow != nullptr)
+		{
+			// Create the SDL renderer...(back buffer)
+			
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, NULL);
+			if (m_pRenderer != nullptr)
+			{
+				// Initialize subsystems later...
+				if (IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG) != 0)
+				{
+					
+				
+				}
+				else return false; // Image initialization failed
+				if (Mix_Init(MIX_INIT_MP3) != 0)
+				{
+					//configured mixer
+					Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 2048);
+					Mix_AllocateChannels(16);
+					//Loads sounds
+
+				}
+				else return false;
+				
+			}
+			else return false; // Renderer creation failed.
+		}
+		else return false; // Window creation failed.
+	}
+	else return false; // initalization failed.
+	m_fps = (Uint32)round(1.0 / (double)FPS * 1000); // Converts FPS into milliseconds, e.g. 16.67
+	m_keystates = SDL_GetKeyboardState(nullptr);
+	cout << "Initialization successful!" << endl;
+	m_running = true;
+	return true;
+}
+
+void Engine::Wake()
+{
+	m_start = SDL_GetTicks();
+}
+
+void Engine::HandleEvents()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			m_running = false;
+			break;
+		case SDL_KEYUP:
+				if (event.key.keysym.sym == ' ')
+				{
+					
+				}
+		}
+	}
+}
+
+bool Engine::KeyDown(SDL_Scancode c)
+{
+	if (m_keystates != nullptr)
+	{
+		if (m_keystates[c] == 1)
+			return true;
+	}
+	return false;
+}
+
+void Engine::Update()
+{	
+	
+}
+
+void Engine::Render()
+{
+	
+}
+
+void Engine::Sleep()
+{
+	m_end = SDL_GetTicks();
+	m_delta = m_end - m_start; // 1055 - 1050 = 5ms
+	if (m_delta < m_fps)
+		SDL_Delay(m_fps - m_delta);
+
+}
+
+int Engine::Run()
+{
+	if (m_running == true)
+	{
+		return 1;
+	}
+	// Start and run the "engine"
+	if (Init("Chrono Dash Build -1.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, NULL) == false)
+	{
+		return 2;
+	}
+	// We passed our initial checks, start the loop!
+	while (m_running == true)
+	{
+
+		Wake();
+		HandleEvents(); // Input
+		Update();       // Processing
+		Render();       // Output
+		if (m_running == true)
+			Sleep();
+	}
+	Clean();
+	return 0;
+}
+
+
+void Engine::Clean()
+{
+	cout << "Cleaning engine..." << endl;
+	Mix_CloseAudio();
+	Mix_Quit();
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_DestroyWindow(m_pWindow);
+	IMG_Quit();
+	SDL_Quit();
+}
+
+
---
+++ GAME1007_SDLTemplate/Engine.h
@@ -0,0 +1,125 @@
+#pragma once
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
+#include<vector>
+#include "SDL_image.h"
+#include "SDL.h"
+#include "SDL_mixer.h"
+#include <iostream>
+#include<time.h>
+#define FPS 120
+#define WIDTH 1024
+#define HEIGHT 768
+using namespace std;
+
+class Sprite
+{
+protected:
+	SDL_Rect m_src;
+	SDL_Rect m_dst;
+public:
+
+	void SetRekts(const SDL_Rect s, const SDL_Rect d)
+	{
+		m_src = s;
+		m_dst = d;
+	}
+	SDL_Rect* GetRectSrc()
+	{
+		return &m_src;
+	}
+	SDL_Rect* GetRectDst()
+	{
+		return &m_dst;
+	}
+	void Update()
+	{
+		m_dst.x -= 2;
+	}
+
+	Sprite()
+	{
+		
+		this->SetRekts({ 0,0,57,62 }, { 1+rand()%1025, 60 + rand() % 600, 57, 62 });
+
+	}
+};
+
+class AnimatedSprite :public Sprite
+{
+private:
+	int m_frame = 0, // "current frame" property
+		m_frameMax = 15, // number of frames to display each sprite
+		m_sprite = 0, // which sprite is on display
+		m_spriteMax = 2; // number of sprites in the animation
+
+public:
+	int m_flip = 0;
+	void SetRects(SDL_Rect s, SDL_Rect d)
+	{
+		m_src = s;
+		m_dst = d;
+	}
+	bool m_faceLeft = 0; // 1 = left | 0 = right
+	void FlipLeft()
+	{
+		if (m_faceLeft == 0)
+			m_faceLeft = 1;
+	}
+	void FlipRight()
+	{
+		if (m_faceLeft == 1)
+			m_faceLeft = 0;
+	}
+
+
+	void Animate()
+	{
+		if (m_sprite >= m_spriteMax)
+		{
+			m_sprite = 0;
+		}
+		if (m_frame == m_frameMax) // if max frames has been hit
+		{
+			m_frame = 0;
+			m_sprite++;
+
+			if (m_sprite == m_spriteMax) // if sprite limit is hit
+			{
+				m_sprite = 0;
+			}
+			m_src.x = m_src.w * m_sprite; // sets the source box location on x axis
+		}
+		m_frame++;
+	}
+};
+
+class Engine
+{
+private: // private properties.
+	bool m_running = false;
+	Uint32 m_start, m_end, m_delta, m_fps;
+	const Uint8* m_keystates;
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+
+
+private: // private method prototypes.
+	int Init(const char* title, int xPos, int yPos, int width, int height, int flags);
+	void Clean();
+	void Wake();
+	void HandleEvents();
+	bool KeyDown(SDL_Scancode c);
+	void Update();
+	void Render();
+	void Sleep();
+	
+
+public: // public method prototypes.
+	int Run();
+};
+
+#endif
+
+// Reminder: you can ONLY have declarations in headers, not logical code
