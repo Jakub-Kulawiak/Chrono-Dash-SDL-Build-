00fc5943f63eb807864931cc055c14f5b3f11d90
Jakub Kulawiak <Jakub.Kulawiak@georgebrown.ca>
2021-03-15 22:46:04 -0400

    touched up level map

--- GAME1017_EmptyEngine/TiledLevel.cpp
+++ GAME1017_EmptyEngine/TiledLevel.cpp
@@ -51,7 +51,7 @@ TiledLevel::TiledLevel(const unsigned short r, const unsigned short c, const int
 			{
 				inFile >> key;
 				m_level[row][col] = m_tiles[key]->Clone(); // Common prototype method.
-				m_level[row][col]->SetXY((float)(col * w), (float)(row * h));
+				m_level[row][col]->SetXY((float)(col * w), (float)(row * h)-832);
 				if (m_level[row][col]->IsObstacle())
 					m_obstacles.push_back(m_level[row][col]);
 			}
